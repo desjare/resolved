62bca6848d4d9b4284054ed349755a887a022617
Eric Desjardins <desjare@gmail.com>
2020-10-10 08:17:29 -0400

    Bug fix. Replace --output by --outputdir.

--- resolved.py
+++ resolved.py
@@ -18,11 +18,10 @@ def run_command(cmd):
         sys.exit(1)
 
 
-def transcode_for_resolve(input_file, output_file):
+def transcode_for_resolve(input_file, output_dir):
 
-    filename, ext = os.path.splitext(input_file)
-    if output_file is not None:
-        output_file = input_file.replace(ext,".mov")
+    _, ext = os.path.splitext(input_file)
+    output_file = os.path.join(output_dir, os.path.basename(input_file.replace(ext,".mov")))
 
     if os.path.exists(output_file):
         print("Skipping existing %s" % (output_file))
@@ -34,9 +33,9 @@ def transcode_for_resolve(input_file, output_file):
     run_command(cmd)
 
 parser = argparse.ArgumentParser(description="resolved.py convert media film so that they can be imported by DaVinci Resolve easily (Registered Trademark of Blackmagic Design Pty Ltd)")
-parser.add_argument("--input", type=str, default=".", help="input file", required=True)
-parser.add_argument("--output", type=str, default=None, help="input file", required=True)
+parser.add_argument("--input", type=str, default=None, dest="input", help="input file", required=True)
+parser.add_argument("--outputdir", type=str, default=None, dest="output_dir", help="output dir", required=True)
 args = parser.parse_args()
 
 
-transcode_for_resolve(args.input, args.output)
+transcode_for_resolve(args.input, args.output_dir)
